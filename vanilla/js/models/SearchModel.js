3a8deaf5f0a73c2978a7e6508d306de911cb8a2c
irostub <irostub@gmail.com>
2021-01-02 10:48:48 +0900

    Implement show result view

--- vanilla/js/models/SearchModel.js
+++ vanilla/js/models/SearchModel.js
@@ -2,12 +2,12 @@ const data = [
   {
     id: 1,
     name: '[키친르쎌] 홈메이드 칠리소스 포크립 650g',
-    image: 'https://cdn.bmf.kr/_data/product/H1821/5a4ed4e8a6751cb1cc089535c000f331.jpg'
+    image: 'https://view01.wemep.co.kr/wmp-deal/1/749/605717491/605717491_large.jpg?1600909614'
   },
   {
     id: 2,
     name: '[키친르쎌] 이탈리아 파티 세트 3~4인분',
-    image: 'https://cdn.bmf.kr/_data/product/H503E/300d931e3b8252ed628b6a3c2f56936b.jpg'
+    image: 'https://lh3.googleusercontent.com/proxy/5QkSFXlVrJZbjpv4N-lS5OHcF9WX6OY-EEVTEyFUW_kzgGInjjYbzZHCDMt1c3j41GAwwmXqqHnZ_u5DW471BXEv-6nBLHfdST27aehUn0KscltXQhg'
   }
 ]
 
