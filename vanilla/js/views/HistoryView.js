b4cff2f642880205e4d999a58710f2dbb66c1ff6
irostub <irostub@gmail.com>
2021-01-04 15:48:07 +0900

    Implement list view in history view

--- vanilla/js/views/HistoryView.js
+++ vanilla/js/views/HistoryView.js
@@ -1,8 +1,20 @@
 import KeywordView from './KeywordView.js'
-import View from './View.js'
 
 const tag = '[HistoryView]'
 
+//keywordView에서 파생
 const HistoryView = Object.create(KeywordView)
+
+//override
+HistoryView.getKeywordsHtml = function(data){
+    return data.reduce((html, item) => {
+        html += `<li data-keyword="${item.keyword}">
+        ${item.keyword}
+        <span class="date">${item.date}</span>
+        <button class="btn-remove"></button>
+        </li>`
+        return html
+    },'<ul class="list">')+'</ul>'
+}
 
 export default HistoryView
