5c5ccd1b90e01012d73aac8bd84bbf83703eb5a8
irostub <irostub@gmail.com>
2021-01-09 06:17:53 +0900

    Implement recommended search view with vuejs

--- vue/js/app.js
+++ vue/js/app.js
@@ -1,4 +1,5 @@
 import SearchModel from './models/SearchModel.js'
+import KeywordModel from './models/KeywordModel.js'
 
 new Vue({
     el: '#app',
@@ -7,11 +8,13 @@ new Vue({
         searchResult: [],
         submitted: false,
         selectedTab:'',
-        tabs: ['추천 검색어', '최근 검색어']
+        tabs: ['추천 검색어', '최근 검색어'],
+        keywords:[]
     },
     //vue 인스턴스가 생성될 때 호출
     created(){
         this.selectedTab=this.tabs[0]
+        this.fetchKeyword()
     },
     methods: {
         onSubmit(e){
@@ -31,9 +34,18 @@ new Vue({
                 this.searchResult = data
             })
         },
+        fetchKeyword(){
+            KeywordModel.list().then(data=>{
+                this.keywords=data
+            })
+        },
         onClickTab(e){
             this.selectedTab=e.currentTarget.innerHTML
             //console.log(this.selectedTab)
+        },
+        onClickKeyword(keyword){
+            this.query=keyword
+            this.search()
         }
     }
 })
