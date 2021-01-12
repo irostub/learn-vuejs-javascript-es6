7e051b67dae318ee818619f9ca778fd37dbfc2d7
irostub <irostub@gmail.com>
2021-01-13 05:43:33 +0900

    Add comment to form component

--- vueComponent/js/components/FormComponent.js
+++ vueComponent/js/components/FormComponent.js
@@ -1,5 +1,7 @@
 export default
 {
+    //query는 다른 컴포넌트에서도 사용을 함으로 더 상위 객체인
+    //부모 vue인스턴스의 query와 fromComponent를 분리해야함.
     //컴포넌트가 바인딩 될 template, dom selector
     template: '#search-form',
     data(){
